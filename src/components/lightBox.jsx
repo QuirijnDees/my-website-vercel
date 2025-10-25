7e93cb605356423a05ed57668fe69076b7c65132
Quirijn Dees <quirijnnn@gmail.com>
2025-10-25 15:17:55 +0200

    Try again 2

--- src/components/lightBox.jsx
+++ src/components/lightBox.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import "./Lightbox.css"; 
+import "./lightbox.css"; 
 
 export default function Lightbox({ imageList = [], currentIndex = 0, onClose }) {
   const [isVisible, setIsVisible] = useState(false);
