fe15cd8a26da5793c2f5267e3ca7405bb117773c
Quirijn Dees <quirijnnn@gmail.com>
2026-02-17 04:22:52 +0100

    rly updated sketches page

--- src/pages/sketches.jsx
+++ src/pages/sketches.jsx
@@ -18,7 +18,7 @@ const subImageList = [
 const gifIndices = []; //////////////// IF AN IMAGE IS A GIF, FILL IN THE IMAGE_(INDEX1)_(INDEX2) PER GIF.
 const headers = [null, "Alien Documentary Sketch"];
 const textParagraphs = [
-    "This page displays a collection of visual sketches and experiments. They are not part of my larger projects, or were scrapped in the process of developing them.\nI still wanted to share them as part of my creative development and skills.",
+    "This page displays a collection of visual sketches and experiments.\nThey are not part of my larger projects, or were scrapped in the process.",
     "This video is a sketch of what a voice-overed timelapse segment in an alien documentary might look and sound like. I created it as an early stage test for an alternative direction for my Master's graduation film Symbionts (2025). The 3D animation is created using Houdini, rendered in Karma,     the voice sampling was done using Supercollider.", 
    ];
 
